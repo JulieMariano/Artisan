f130a5394e257acd12c15ec588ce6dfe54609279
Thomas Gibson <gibsonjthomas96@gmail.com>
2020-06-22 18:32:05 +0100

    commiting final saves - nothing editied from me since last commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,7 @@
 class CategoriesController < ApplicationController
-<<<<<<< HEAD
   skip_before_action :authenticate_user!, only: [:index]
   def index
-=======
-  def index
     @categories = Category.all
->>>>>>> master
+
   end
 end
