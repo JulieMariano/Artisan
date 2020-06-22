10e406c33361b081b2e60b0edab7f6097842082d
Laure Olivier <laurelacombeolivier@gmail.com>
2020-06-22 17:31:34 +0100

    creating the controller actions and views for admin items

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,46 @@
+class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    @item.save
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def patch
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path(@item)
+  end
+
+  def delete
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :picture, :description, :category_id )
+  end
+
+
+end
