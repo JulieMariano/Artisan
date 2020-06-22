20beb006c90f794a543877cd2c95d8dd826f3878
Julie Mariano <j.mariano@live.be>
2020-06-22 15:38:50 +0100

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  root to: 'categories#index'
+  
+  namespace :admin do
+    resources :items do
+      resources :orders
+    end
+  end
+
+  resources :categories do
+    resources :items, only: [:index, :show] do
+      resources :orders, only: [:create, :destroy]
+    end
+  end
+
+  # get 'my_orders', to: 'orders#my_orders' 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
