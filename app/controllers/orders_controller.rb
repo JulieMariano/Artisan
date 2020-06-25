89a949fe0635e787e71e94a4dee46a7639cd7825
Edgar Marques <edgar.eu1@hotmail.com>
2020-06-25 15:20:22 +0100

    Make it prettier

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,7 @@ class OrdersController < ApplicationController
     @order.user = current_user
 
     if @order.save
-      redirect_to category_item_path(@category, @item)
+      redirect_to category_item_path(@category, @item), notice: "Order for #{@item.name} was created!"
     else
       render 'items/show'
     end
