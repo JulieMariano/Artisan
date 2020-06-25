f882273dd278fd949aa92208a769829cdd64bc2d
Edgar Marques <edgar.eu1@hotmail.com>
2020-06-25 01:21:02 +0100

    Display a list of the reviews for the item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @user = User.find(current_user.id)
+    @user = User.find(current_user.id) if user_signed_in?
     @category = Category.find(params[:category_id])
     @item = Item.find(params[:id])
     @review = Review.new
