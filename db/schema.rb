90e24f307c80342c5e9a5db427289875c2c7e03f
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-14 02:04:00 +0100

    Add the Checkout Session Id column to the Orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_003809) do
+ActiveRecord::Schema.define(version: 2020_09_14_010124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_003809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "state"
+    t.string "checkout_session_id"
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
