1d31dbb38b46278858a7fff659a780f1878f7b09
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-20 23:27:08 +0100

    Add validations to the name column of the Devise/User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+
   has_many :items, through: :orders
   has_many :orders
   has_many :reviews
