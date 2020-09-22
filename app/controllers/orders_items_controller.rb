30f4bf5550c4fba0124a60579c6309b2952f382d
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-22 20:01:19 +0100

    Ajaxify the OrdersItems#destroy action

--- app/controllers/orders_items_controller.rb
+++ app/controllers/orders_items_controller.rb
@@ -7,8 +7,6 @@ class OrdersItemsController < ApplicationController
   def destroy
     @orders_item = OrdersItem.find(params[:id])
     @orders_item.destroy
-
-    redirect_to orders_path
   end
 
   private
