c3a13639f380bbd05c22fa950ad3cfc58e65a37c
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-22 01:52:59 +0100

    Change the 'items' column from Orders into an array of hashes of Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_225739) do
+ActiveRecord::Schema.define(version: 2020_09_21_234429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_225739) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "state"
     t.string "checkout_session_id"
-    t.text "items"
+    t.text "items", default: [], array: true
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
