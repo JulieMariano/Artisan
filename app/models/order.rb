571223059a52b4fb3e682db323a059a9dc1f7bec
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-23 15:28:32 +0100

    Create the Order methods for quantifying the shipping and total costs

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,13 +6,35 @@ class Order < ApplicationRecord
   has_many :orders_items
   has_many :items, through: :orders_items
 
-  # Get the total to pay for the items in the order
+  # Returns the number of items of the order
+  def num_items
+    orders_items.inject(0) { |sum, order_item| sum += order_item.quantity }
+  end
+
+  # Returns the cost of the items of the order
   def subtotal
     orders_items.inject(0) { |sum, order_item| sum += order_item.subtotal }
   end
 
-  # Get the number of items in the order
-  def num_items
-    orders_items.inject(0) { |sum, order_item| sum += order_item.quantity }
+  # Returns the shipping cost of the order
+  def shipping
+    items_subtotal = subtotal.to_i
+
+    if items_subtotal < 100
+      result = [3.5, items_subtotal * 0.05].max
+
+    elsif items_subtotal < 500
+      result =  [5.5, items_subtotal * 0.03].max
+
+    else
+      result = [16, items_subtotal * 0.02].max
+    end
+
+    return Money.new(result * 100)
+  end
+
+  # Returns the total cost of the order
+  def total
+    subtotal + shipping
   end
 end
