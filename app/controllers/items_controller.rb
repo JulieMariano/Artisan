2c23793122e3ee067568d5b247a3ceba2cfeab05
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-24 22:39:30 +0100

    Refactor the Items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
 
   def index
     @category = Category.find(params["category_id"])
+    @category_items = @category.items_by_relevance
   end
 
   def show
@@ -10,6 +11,5 @@ class ItemsController < ApplicationController
     @category = Category.find(params[:category_id])
     @item = Item.find(params[:id])
     @review = Review.new
-    @order = Order.new
   end
 end
