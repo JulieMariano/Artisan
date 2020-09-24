ef5471c3541aa38e15fca34ad94e6e49670acd5b
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-24 22:40:00 +0100

    Refactor the OrdersItems controller

--- app/controllers/orders_items_controller.rb
+++ app/controllers/orders_items_controller.rb
@@ -3,16 +3,16 @@ class OrdersItemsController < ApplicationController
     orders_item = OrdersItem.find(params[:id])
     orders_item.update(orders_item_params) if orders_item_params[:quantity].to_i.positive?
 
-    @order = current_user.pending_order
-    @order_items = @order.orders_items.order(created_at: :desc)
+    @order = current_user.cart
+    @order_items = @order.orders_items.order(updated_at: :desc)
   end
 
   def destroy
     orders_item = OrdersItem.find(params[:id])
     orders_item.destroy
 
-    @order = current_user.pending_order
-    @order_items = @order.orders_items.order(created_at: :desc)
+    @order = current_user.cart
+    @order_items = @order.orders_items.order(updated_at: :desc)
   end
 
   private
