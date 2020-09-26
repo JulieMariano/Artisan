cb46f5e7b7bba001f4c508c01f19ab24e6b6e919
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-26 15:41:09 +0100

    Create the JS smoothPagination function

---
+++ app/javascript/components/smooth-pagination.js
@@ -0,0 +1,24 @@
+// Function that makes the pagination anchors scroll up to the beginning of the Paid Orders list
+function smoothPagination() {
+  const anchors = document.getElementsByClassName("page-link"); // Get the anchors of the pagination
+
+  // For each of the anchors do...
+  for (let anchor of anchors) {
+    const anchorRef = anchor.href;
+
+    // Make sure the anchor has a page destination
+    if (typeof anchorRef !== "undefined") {
+      anchor.setAttribute("data-remote", "true"); // Ajaxify the anchor
+
+      // Upon clicking on the anchor...
+      anchor.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the anchor's default click behavior
+
+        // Use jQuery to perform a scrolling animation to the top of the Paid Orders list
+        $("html, body").animate( {
+          scrollTop: $('#paid-orders').offset().top - 68  // 68 is the height of the navbar
+        }, 250);
+      });
+    }
+  }
+}
