eed2763c735afb70266cd2fd15651d108ab7cfe9
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-27 18:32:54 +0100

    Schedule the delivery to 3 days after payment, using a Background Job

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,5 +3,8 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid', payment_date: DateTime.now)
+
+    # Schedule the delivery to be made 3 days after the payment, using a Background Job
+    DeliveryJob.set(wait: 3.days).perform_later(order.id)
   end
 end
