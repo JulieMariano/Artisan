6f9c01e68fdd181aef1873b368c55c113e0197e0
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-29 00:50:31 +0100

    Refactor and clean the Admin/Categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,14 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: [:update, :show, :destroy]
+
   def index
     @categories = Category.all
   end
 
-  def new
-    @category = Category.new
-  end
-
   def create
     @category = Category.new(category_params)
+
     if @category.save
       redirect_to admin_categories_path
     else
@@ -16,23 +15,29 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
-
   def update
-    @category = Category.find(params[:id])
     @category.update(category_params)
+
     redirect_to admin_categories_path
   end
 
   def show
-    @category = Category.find(params[:id])
     @items = Item.where(category: @category)
   end
 
+  def destroy
+    @category.destroy
+
+    redirect_to admin_categories_path
+  end
+
   private
+
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
