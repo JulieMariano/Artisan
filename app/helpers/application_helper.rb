d423df3ba750b0904480f40d16eef5a1621b457c
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-28 16:38:08 +0100

    Create an helper to display the User avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Method that displays the user avatar or, if this isn't present, the standard avatar
+  def small_avatar(user)
+    if user.avatar.attached?
+      cl_image_tag(user.avatar.key, transformation: [
+        { width: 250, height: 250, gravity: 'face', radius: 'max', crop: 'fill' }
+      ], class: "avatar")
+    else
+      image_tag('default-user-avatar.png', class: "avatar")
+    end
+  end
 end
