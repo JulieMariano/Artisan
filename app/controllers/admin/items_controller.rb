f5d2ca0de75cae9a1b88c43933be90152f77ee6d
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-29 00:50:47 +0100

    Refactor and clean the Admin/Items controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,56 +1,37 @@
 class Admin::ItemsController < ApplicationController
-
-  # def index
-  #   @category = Category.find(params[:category_id])
-  #   @items = Item.where(category: @category)
-  # end
-
-  # def show
-  #   @category = Category.find(params[:category_id])
-  #   @item = Item.find(params[:id])
-  # end
-
-  def new
-    @category = Category.find(params[:category_id])
-    @item = Item.new
-    @admin = current_user
-  end
+  before_action :set_category, only: [:create, :update, :destroy]
 
   def create
-    @category = Category.find(params[:category_id])
     @item = Item.create(item_params)
+
     if @item.save
       redirect_to admin_category_items_path
     else
       render :new
     end
   end
-
-
-  def edit
-    @category = Category.find(params[:category_id])
-    @item = Item.find(params[:id])
-  end
   
   def update
-    @category = Category.find(params[:category_id])
     @item = Item.find(params[:id])
     @item.update(item_params)
+
     redirect_to admin_category_items_path(@category)
   end
 
   def destroy
-    @category = Category.find(params[:category_id])
     @item = Item.find(params[:id])
     @item.destroy
+
     redirect_to admin_category_items_path(@category)
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :picture, :category_id )
+    params.require(:item).permit(:name, :price, :description, :picture, :category_id)
   end
 
-
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
 end
