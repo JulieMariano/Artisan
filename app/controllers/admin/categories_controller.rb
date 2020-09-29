5ead180810af37c8f3040ea6d9482d01f04f2f51
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-29 19:06:05 +0100

    Ajaxify the Admin/Categories#create action

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,12 +7,7 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-
-    if @category.save
-      redirect_to admin_categories_path
-    else
-      render :new
-    end
+    @category.save
   end
 
   def update
