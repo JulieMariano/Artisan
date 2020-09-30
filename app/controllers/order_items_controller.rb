fc25f6aa6344e8708d951f9b02349dd00846c177
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-30 19:55:53 +0100

    Refactor, clean and comment the controllers/order_items code

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,23 +1,24 @@
 class OrderItemsController < ApplicationController
-  def update
-    order_item = OrderItem.find(params[:id])
-    order_item.update(order_item_params) if order_item_params[:quantity].to_i.positive?
+  before_action :set_order_variables, only: [:update, :destroy]
 
-    @order = current_user.cart
-    @order_items = @order.order_items.order(created_at: :desc)
+  def update
+    # Update it only if the quantity param is positive
+    @order_item.update(order_item_params) if order_item_params[:quantity].to_i.positive?
   end
 
   def destroy
-    order_item = OrderItem.find(params[:id])
-    order_item.destroy
-
-    @order = current_user.cart
-    @order_items = @order.order_items.order(created_at: :desc)
+    @order_item.destroy
   end
 
   private
 
   def order_item_params
     params.require(:order_item).permit(:quantity)
+  end
+
+  def set_order_variables
+    @order = current_user.cart
+    @order_items = @order.order_items.order(created_at: :desc)
+    @order_item = @order_items.find(params[:id])
   end
 end
